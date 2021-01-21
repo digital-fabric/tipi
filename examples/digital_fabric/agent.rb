3413a54fca733fdda03efc4cfc5d34801a800ee6
Sharon Rosner <sharon@noteflakes.com>
2021-01-21 10:26:02 +0000

    Spawn independent agent instead of forking

---
+++ examples/digital_fabric/agent.rb
@@ -0,0 +1,59 @@
+# frozen_string_literal: true
+
+require 'bundler/setup'
+require 'polyphony'
+require 'json'
+require 'tipi/digital_fabric/protocol'
+require 'tipi/digital_fabric/agent'
+
+Protocol = Tipi::DigitalFabric::Protocol
+
+class SampleAgent < Tipi::DigitalFabric::Agent
+  def initialize(id, host, port)
+    @id = id
+    super(host, port, { host: "#{id}.realiteq.net" }, 'foobar')
+    @name = "agent-#{@id}"
+  end
+
+  def http_request(req)
+    send_df_message(Protocol.http_response(
+      req['id'],
+      nil,
+      { 'Content-Type': 'text/json' },
+      false
+    ))
+
+    10.times do
+      sleep 1
+      do_some_activity
+      send_df_message(Protocol.http_response(
+        req['id'],
+        { id: @id, time: Time.now.to_i }.to_json,
+        nil,
+        false
+      ))
+    end
+  
+    send_df_message(Protocol.http_response(
+      req['id'],
+      nil,
+      nil,
+      true
+    ))
+  end
+
+  def do_some_activity
+    File.open('/tmp/df-test.log', 'a+') { |f| sleep rand; f.puts "#{Time.now} #{@name} #{generate_data(2**8)}" }
+  end
+
+  def generate_data(length)
+    charset = Array('A'..'Z') + Array('a'..'z') + Array('0'..'9')
+    Array.new(length) { charset.sample }.join
+  end
+end
+
+id = ARGV[0]
+puts "Starting agent #{id} pid: #{Process.pid}"
+
+spin_loop(interval: 60) { GC.start }
+SampleAgent.new(id, '127.0.0.1', 4411).run
--- examples/digital_fabric/multi_agent_supervisor.rb
+++ examples/digital_fabric/multi_agent_supervisor.rb
@@ -6,52 +6,56 @@ require 'json'
 require 'tipi/digital_fabric/protocol'
 require 'tipi/digital_fabric/agent'
 
-Protocol = Tipi::DigitalFabric::Protocol
+# Protocol = Tipi::DigitalFabric::Protocol
 
-class SampleAgent < Tipi::DigitalFabric::Agent
-  def initialize(id, host, port)
-    @id = id
-    super(host, port, { host: "#{id}.realiteq.net" }, 'foobar')
-    @name = "agent-#{@id}"
-  end
+# class SampleAgent < Tipi::DigitalFabric::Agent
+#   def initialize(id, host, port)
+#     @id = id
+#     super(host, port, { host: "#{id}.realiteq.net" }, 'foobar')
+#     @name = "agent-#{@id}"
+#   end
 
-  def http_request(req)
-    response = { id: @id, time: Time.now.to_i }
-    do_some_activity
-    # log "request: #{req.inspect}"
-    send_df_message(Protocol.http_response(
-      req['id'],
-      response.to_json,
-      { 'Content-Type': 'text/json' },
-      true
-    ))
-  end
+#   def http_request(req)
+#     response = { id: @id, time: Time.now.to_i }
+#     do_some_activity
+#     # log "request: #{req.inspect}"
+#     send_df_message(Protocol.http_response(
+#       req['id'],
+#       response.to_json,
+#       { 'Content-Type': 'text/json' },
+#       true
+#     ))
+#   end
 
-  def do_some_activity
-    @data = generate_data(2 ** 10)
-    File.open('/tmp/df-test.log', 'a+') { |f| sleep rand; f.puts "#{Time.now} #{@name} #{generate_data(20)}" }
-  end
+#   def do_some_activity
+#     @data = generate_data(2 ** 10)
+#     File.open('/tmp/df-test.log', 'a+') { |f| sleep rand; f.puts "#{Time.now} #{@name} #{generate_data(20)}" }
+#   end
 
-  def generate_data(length)
-    charset = Array('A'..'Z') + Array('a'..'z') + Array('0'..'9')
-    Array.new(length) { charset.sample }.join
-  end
-end
+#   def generate_data(length)
+#     charset = Array('A'..'Z') + Array('a'..'z') + Array('0'..'9')
+#     Array.new(length) { charset.sample }.join
+#   end
+# end
+
+require 'fileutils'
+FileUtils.cd(__dir__)
 
 def spin_agent(id)
   spin do
     while true
-      Polyphony::Process.watch do
-        puts "Agent #{id} pid: #{Process.pid}"
+      Polyphony::Process.watch("ruby agent.rb #{id}")
+      # Polyphony::Process.watch do
+      #   puts "Agent #{id} pid: #{Process.pid}"
 
-        spin_loop(interval: 60) { GC.start }
+      #   spin_loop(interval: 60) { GC.start }
       
-        agent = SampleAgent.new(id, '127.0.0.1', 4411)
-        agent.run
-      rescue Interrupt
-        # die quietly
-      end
-      sleep 5
+      #   agent = SampleAgent.new(id, '127.0.0.1', 4411)
+      #   agent.run
+      # rescue Interrupt
+      #   # die quietly
+      # end
+      sleep 1
     end
   end
 end
